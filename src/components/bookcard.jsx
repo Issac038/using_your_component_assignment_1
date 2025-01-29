1ea036651777f22996814aa7127af043a770c9cc
Issac038 <issac.antony@kalvium.community>
2025-01-29 11:52:48 +0530

    added the bookcard component

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,15 @@
 // write the book component code here
+import './bookcard.css';
+
+const BookCard = ({ image, name, genre, author }) => {
+  return (
+    <div className="book-card">
+      <img src={image} alt={name} className="book-image" />
+      <h3 className="book-name">{name}</h3>
+      <p className="book-genre"><strong>Genre:</strong> {genre}</p>
+      <p className="book-author"><strong>Author:</strong> {author}</p>
+    </div>
+  );
+};
+
+export default BookCard;
